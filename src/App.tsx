57f25f5ed6d96af7c0a99c3b6256ec63f7066b56
KarolKarnas <karol.karnas@gmail.com>
2023-11-12 13:31:58 +0100

    refactor: Enhance data handling and simplify calculations
    - Limit quantity of useState
    - Better loading/error handling
    - Simplify calculations

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import './App.css';
 import CalendarSummary from './CalendarSummary/CalendarSummary';
 
 function App() {
-	return (
-		<div
-			className='App'
-			style={{ maxWidth: '1000px', margin: 'auto', padding: '0 1rem' }}
-		>
-			<CalendarSummary />
-		</div>
-	);
+  return (
+    <div className="App" style={{ maxWidth: '1000px', margin: 'auto', padding: "0 1rem" }}>
+      <CalendarSummary />
+    </div>
+  );
 }
 
 export default App;
--- src/CalendarSummary/CalendarSummary.tsx
+++ src/CalendarSummary/CalendarSummary.tsx
@@ -1,105 +1,107 @@
 import React, { useEffect, useState } from 'react';
-import './CalendarSummary.css'
+import './CalendarSummary.css';
 import { getCalendarEvents } from '../api-client';
 import { CalendarEvent } from '../api-client';
 
-interface DailyEvent {
+interface DailyEvents {
 	date: Date;
-	calendarEvents: CalendarEvent[];
+	eventsNumber: number;
+	totalDuration: number;
+	longestEvent: string;
 }
 
 const CalendarSummary: React.FunctionComponent = () => {
-	const [dailyEvents, setDailyEvents] = useState<DailyEvent[] | null>(null);
-	const [totalPeriodEventsCount, setTotalPeriodEventsCount] =
-		useState<number>(0);
-	const [totalPeriodDuration, setTotalPeriodDuration] = useState<number>(0);
-	const [longestPeriodEvents, setLongestPeriodEvents] = useState<
-		CalendarEvent[] | null
-	>(null);
+	const [dailyEvents, setDailyEvents] = useState<DailyEvents[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [isError, setIsError] = useState<boolean>(false);
+	// const [totalPeriodEventsCount, setTotalPeriodEventsCount] =
+	// 	useState<number>(0);
+	// const [totalPeriodDuration, setTotalPeriodDuration] = useState<number>(0);
+	// const [longestPeriodEvents, setLongestPeriodEvents] = useState<
+	// 	CalendarEvent[] | null
+	// >(null);
 
 	useEffect(() => {
-		fetchData(7).then((data) => {
-			// console.log(data);
-			setDailyEvents(data);
-
-			const totalPeriodEventsCount = data.reduce((acc, curr) => {
-				return acc + getTotalDailyEvents(curr);
-			}, 0);
-			setTotalPeriodEventsCount(totalPeriodEventsCount);
-
-			//total weekly duration
-			const totalPeriodDuration = data.reduce((acc, curr) => {
-				return acc + getTotalDailyDuration(curr);
-			}, 0);
-			setTotalPeriodDuration(totalPeriodDuration);
-
-			// longest weekly
-			const longestPeriodEvents = getLongestPeriodEvents(data);
-			setLongestPeriodEvents(longestPeriodEvents);
-		});
+		fetchData(7);
 	}, []);
 
 	const fetchData = async (daysNumber: number) => {
-		const fetchDailyEvents: DailyEvent[] = [];
-		// loop through number of required days
-		for (let index = 0; index < daysNumber; index++) {
-			let date = new Date();
-			date.setDate(date.getDate() + index);
-			try {
+		try {
+			setIsLoading(true);
+			const fetchDailyEvents: DailyEvents[] = [];
+			// loop through number of required days
+			for (let index = 0; index < daysNumber; index++) {
+				let date = new Date();
+				date.setDate(date.getDate() + index);
 				const calendarEvents = await getCalendarEvents(date);
 
-				// store calendarEvents and the date in new object type DailyEvent
-				const calendarEventDate: DailyEvent = {
+				const longestEvent = calendarEvents.reduce((longest, current) =>
+					current.durationInMinutes > longest.durationInMinutes
+						? current
+						: longest
+				);
+
+				// store calendarEvents and the date in new object type DailyEvents
+				const calendarEventDate: DailyEvents = {
 					date,
-					calendarEvents,
+					eventsNumber: calendarEvents.length,
+					totalDuration: calendarEvents.reduce(
+						(acc, curr) => acc + curr.durationInMinutes,
+						0
+					),
+					longestEvent: longestEvent.title,
 				};
 				fetchDailyEvents.push(calendarEventDate);
-			} catch (error) {
-				console.error('Error fetching events:', error);
 			}
-		}
-		return fetchDailyEvents;
+			setDailyEvents(fetchDailyEvents);
+			setIsLoading(false);
+			return;
+		} catch (error) {
+			console.error('Error fetching events:', error);
+		} 
 	};
 
-	const getTotalDailyDuration = (day: DailyEvent) => {
-		return day.calendarEvents.reduce(
-			(acc, curr) => acc + curr.durationInMinutes,
-			0
-		);
-	};
+	// const getTotalDailyDuration = (day: DailyEvent) => {
+	// 	return day.calendarEvents.reduce(
+	// 		(acc, curr) => acc + curr.durationInMinutes,
+	// 		0
+	// 	);
+	// };
 
-	const getTotalDailyEvents = (day: DailyEvent) => {
-		return day.calendarEvents.length;
-	};
+	// const getTotalDailyEvents = (day: DailyEvent) => {
+	// 	return day.calendarEvents.length;
+	// };
 
-	const getLongestPeriodEvents = (data: DailyEvent[]) => {
-		let longestDuration = 0;
-		// search for the longest weekly event
-		data.forEach((dayEvents) => {
-			dayEvents.calendarEvents.forEach((event) => {
-				if (event.durationInMinutes > longestDuration) {
-					longestDuration = event.durationInMinutes;
-				}
-			});
-		});
+	// const getLongestPeriodEvents = (data: DailyEvent[]) => {
+	// 	let longestDuration = 0;
+	// 	// search for the longest weekly event
+	// 	data.forEach((dayEvents) => {
+	// 		dayEvents.calendarEvents.forEach((event) => {
+	// 			if (event.durationInMinutes > longestDuration) {
+	// 				longestDuration = event.durationInMinutes;
+	// 			}
+	// 		});
+	// 	});
 
-		// check if there are more than one event with the longestDuration
-		const longestEvents: CalendarEvent[] = [];
-		data.forEach((dayEvents) => {
-			dayEvents.calendarEvents.forEach((event) => {
-				if (event.durationInMinutes === longestDuration) {
-					longestEvents.push(event);
-				}
-			});
-		});
-		return longestEvents;
-	};
+	// 	// check if there are more than one event with the longestDuration
+	// 	const longestEvents: CalendarEvent[] = [];
+	// 	data.forEach((dayEvents) => {
+	// 		dayEvents.calendarEvents.forEach((event) => {
+	// 			if (event.durationInMinutes === longestDuration) {
+	// 				longestEvents.push(event);
+	// 			}
+	// 		});
+	// 	});
+	// 	return longestEvents;
+	// };
 
 	return (
 		<div>
 			<h2>Calendar summary</h2>
-			{dailyEvents === null ? (
+			{isLoading ? (
 				<div>Loading...</div>
+			) : isError ? (
+				<div>Error</div>
 			) : (
 				<table>
 					<thead>
@@ -110,7 +112,7 @@ const CalendarSummary: React.FunctionComponent = () => {
 							<th scope='col'>Longest event </th>
 						</tr>
 					</thead>
-					<tbody>
+					{/* <tbody>
 						{dailyEvents.map((day, index) => (
 							<tr key={index}>
 								<td>{day.date.toISOString().split('T')[0]}</td>
@@ -148,7 +150,7 @@ const CalendarSummary: React.FunctionComponent = () => {
 								) : null}
 							</td>
 						</tr>
-					</tbody>
+					</tbody> */}
 				</table>
 			)}
 		</div>
